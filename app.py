22286842a207ef10b288b198bafe8127cfa5560f
andan2 <136687012+andan2@users.noreply.github.com>
2024-02-25 01:11:19 +0100

    primer commit

---
+++ app.py
@@ -0,0 +1,106 @@
+# Importing required packages
+import streamlit as st
+import openai
+import uuid
+import time
+from openai import OpenAI
+print(openai.VERSION)
+
+# Initialize OpenAI client
+client = OpenAI()
+
+# Your chosen model
+MODEL = "gpt-4-turbo-preview"
+
+# Initialize session state variables
+if "session_id" not in st.session_state:
+    st.session_state.session_id = str(uuid.uuid4())
+
+if "run" not in st.session_state:
+    st.session_state.run = {"status": None}
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if "retry_error" not in st.session_state:
+    st.session_state.retry_error = 0
+
+# Set up the page
+st.set_page_config(page_title="Slinki | Tu Guía turístico personal")
+
+# ---- SIDEBAR ----
+with open("ui/sidebar.md", "r", encoding="utf-8") as sidebar_file:
+    sidebar_content = sidebar_file.read()
+    url = "https://www.tiktok.com/@planetaandrei"
+st.sidebar.markdown(f"<a href='{url}' target='_blank'><button style='color: white; background-color: #4255f4; padding: 10px 20px; border-radius: 5px; border: none; cursor: pointer;'>Sígueme en TikTok</button></a>", unsafe_allow_html=True)
+st.sidebar.image('./imagenes/slinkibot.png')
+st.sidebar.markdown(sidebar_content)
+
+# Initialize OpenAI assistant
+if "assistant" not in st.session_state:
+    openai.api_key = st.secrets["OPENAI_API_KEY"]
+    st.session_state.assistant = openai.beta.assistants.retrieve(st.secrets["OPENAI_ASSISTANT"])
+    st.session_state.thread = client.beta.threads.create(
+        metadata={'session_id': st.session_state.session_id}
+    )
+
+# Display chat messages
+elif hasattr(st.session_state.run, 'status') and st.session_state.run.status == "completed":
+    st.session_state.messages = client.beta.threads.messages.list(
+        thread_id=st.session_state.thread.id
+    )
+    for message in reversed(st.session_state.messages.data):
+        if message.role in ["user", "assistant"]:
+            with st.chat_message(message.role):
+                for content_part in message.content:
+                    message_text = content_part.text.value
+                    st.markdown(message_text)
+
+# Chat input and message creation
+if prompt := st.chat_input("¿por qué no me lanzas un hueso... digo, una pregunta?"):
+    with st.chat_message('user'):
+        st.write(prompt)
+
+    message_data = {
+        "thread_id": st.session_state.thread.id,
+        "role": "user",
+        "content": prompt
+    }
+
+    st.session_state.messages = client.beta.threads.messages.create(**message_data)
+
+    st.session_state.run = client.beta.threads.runs.create(
+        thread_id=st.session_state.thread.id,
+        assistant_id=st.session_state.assistant.id,
+    )
+    if st.session_state.retry_error < 3:
+        time.sleep(1)
+        st.rerun()
+
+# Handle run status
+if hasattr(st.session_state.run, 'status'):
+    if st.session_state.run.status == "running":
+        with st.chat_message('assistant'):
+            st.write("Thinking ......")
+        if st.session_state.retry_error < 3:
+            time.sleep(1)
+            st.rerun()
+
+    elif st.session_state.run.status == "failed":
+        st.session_state.retry_error += 1
+        with st.chat_message('assistant'):
+            if st.session_state.retry_error < 3:
+                st.write("Run failed, retrying ......")
+                time.sleep(3)
+                st.rerun()
+            else:
+                st.error("FAILED: The OpenAI API is currently processing too many requests. Please try again later ......")
+
+    elif st.session_state.run.status != "completed":
+        st.session_state.run = client.beta.threads.runs.retrieve(
+            thread_id=st.session_state.thread.id,
+            run_id=st.session_state.run.id,
+        )
+        if st.session_state.retry_error < 3:
+            time.sleep(3)
+            st.rerun()
---
+++ ui/sidebar.md
@@ -0,0 +1,13 @@
+# Slinki
+
+¡Hola! Soy **Slinki**, un perro aventurero con sombrero y mucho más que un compañero de viajes. Diseñado por **Andrei**, estoy aquí para ser tu **guía turístico personal** en la búsqueda de las mejores experiencias. Los perros son reconocidos por su habilidad de rastreo, y yo, como tu amigo explorador, prometo llevarte por las sendas menos transitadas para descubrir tours gratuitos increíbles en ciudades de todo el mundo. ¿Qué tal si juntos descubrimos las maravillas que nos esperan?
+
+## ¿Cómo Puedo Ayudarte?
+
+- **Tours Gratuitos**: Te mostraré las mejores opciones de tours sin costo en cualquier ciudad.
+- **Consejos de Viaje**: Puedo ofrecerte recomendaciones personalizadas para tu aventura.
+- **Planificación de Rutas**: Te ayudaré a organizar tu itinerario para maximizar tu experiencia.
+- **Compañía y Diversión**: Estoy aquí para hacer de tu viaje una experiencia memorable.
+- **Curiosidades y Cultura**: ¿Interesado en aprender más sobre los destinos? Te proporcionaré datos fascinantes para enriquecer tu viaje.
+
+Recuerda, en mí no solo encontrarás una guía, sino un **compañero entusiasta** que siempre estará dispuesto a llevarte a descubrir. ¿Listo para comenzar esta emocionante aventura juntos?
